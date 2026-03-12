9514cf40e1cf879cb7d98856cf1d802b13f98342
Alex Ross <alextross15@gmail.com>
2026-03-11 21:17:16 -0400

    Add user registration endpoint

---
+++ backend/Coiny.Api/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+using Coiny.Api.Entities;
+using Coiny.Api.DTOs.Auth;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coiny.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AuthController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = request.Email,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName
+            };
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            return Ok("User registered successfully.");
+        }
+    }
+}
